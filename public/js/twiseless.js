4f1afd427c61e3921a726da6b2931490453df780
root <root@bananapi.(none)>
2016-04-09 18:04:27 +0200

    Adding temperature chart

--- public/js/twiseless.js
+++ public/js/twiseless.js
@@ -1,6 +1,52 @@
 (function($) {
-$.getJSON("/last_24_hours", function(data) {
-    alert('yes');
-});
+	google.charts.load('current', {'packages': ['corechart']});
+	google.charts.setOnLoadCallback(drawChart);
+
+	function drawChart() {
+		$.getJSON("/last_24_hours", function(data) {
+			var temperatureArray = [ ];
+			var heaterTemperature = [];
+			var airTemperature = [];
+
+			jQuery.each(data, function(index, value) {
+				heaterTemperature.push(value.heaterTemperature);
+				airTemperature.push(value.airTemperature);
+				temperatureArray.push([value.readingDateTime, value.airTemperature, value.heaterTemperature]);
+			});
+
+			var lowestAirTemperature = Math.min.apply(Math, airTemperature);
+			var highestAirTemperature = Math.max.apply(Math, airTemperature);
+
+			var lowestHeaterTemperature = Math.min.apply(Math, heaterTemperature);
+			var highestHeaterTemperature = Math.max.apply(Math, heaterTemperature);
+
+			var lowestTemperature = lowestAirTemperature > lowestHeaterTemperature ? lowestHeaterTemperature - 2 : lowestAirTemperature - 2;
+			var highestTemperature = highestAirTemperature > highestHeaterTemperature ? highestAirTemperature + 2 : highestHeaterTemperature + 2;
+
+			var ticksArray = [];
+			for(var i=lowestTemperature; i<=highestTemperature; i++) {
+				ticksArray.push(i);
+			}
+
+			var chartData = new google.visualization.DataTable();
+			chartData.addColumn('string', 'Data');
+			chartData.addColumn('number', 'Temperaturza powierza');
+			chartData.addColumn('number', 'Temperaturza grzejnika');
+
+			chartData.addRows(temperatureArray);
+			
+			var options = {
+				title: 'Temperatura z 24 godzin',
+				curveType: 'function',
+				legend: { position: 'bottom' },
+				width: 1400,
+				height: 700,
+				vAxis: { minValue: lowestTemperature, maxValue: highestTemperature, ticks: ticksArray },
+			};
+
+			var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+			chart.draw(chartData, options);
+		});
+	}
 
 })(jQuery);
--- template/index.mako
+++ template/index.mako
@@ -3,22 +3,17 @@
   <head>
     <title>Home Temperature</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <script type="text/javascript" src="/static/js/jquery-1.7.2.min.js"></script> 
-    <script type="text/javascript" src="/static/js/twiseless.js"></script> 
-    <script type="text/javascript">
-      $(document).ready(function() {
-        $("#viz").twiseless("render");
-      });
-    </script>
+	<script type="text/javascript" src="/static/js/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<script type="text/javascript" src="/static/js/twiseless.js"></script>
   </head>
   <body>
     <div id="container">
       <div id="header">
-	<h1>Home Temperature</h1>
+		<h1>Home Temperature</h1>
       </div>
       <div id="content-container">
-	<div id="content">
-	</div>
+		<div id="curve_chart"></div>
       </div>
     </div>
   </body>
